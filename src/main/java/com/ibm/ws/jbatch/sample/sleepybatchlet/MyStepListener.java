2b445b4ec9006bfa1009e8392fb413ab51287108
Scott Kurz <skurz@us.ibm.com>
2021-12-05 07:49:18 -0500

    use https repo;  final version
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

---
+++ src/main/java/com/ibm/ws/jbatch/sample/sleepybatchlet/MyStepListener.java
@@ -0,0 +1,33 @@
+package com.ibm.ws.jbatch.sample.sleepybatchlet;
+
+import javax.batch.api.listener.AbstractStepListener;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+//@ApplicationScoped
+@Dependent
+public class MyStepListener extends AbstractStepListener {
+
+	private int count = 0;
+
+	public void getCount() {
+		System.out.println("SKSK: in step listener , cnt = " + ++count);
+	}
+	
+//    @Inject SleepyBatchlet batchlet;
+    @Inject MyCounterBean counter;
+    
+	@Override
+	public void beforeStep() throws Exception {
+		System.out.println("SKSK: in step listener before,  cnt = " + ++count);
+//		batchlet.getCount();
+//		System.out.println("SKSK: in step listener before,  date =   " + batchlet.getDate());
+	}
+
+	@Override
+	public void afterStep() throws Exception {
+		System.out.println("SKSK: in step listener after, cnt = " + ++count);
+//		batchlet.getCount();
+	}
+}
